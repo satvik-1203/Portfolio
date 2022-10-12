27da5b52786bee32ac35b08787e359b677e98607
satvik-1203 <satvikgsu@gmail.com>
2022-10-12 16:30:14 -0400

    image bug

--- pages/api/metaTag.tsx
+++ pages/api/metaTag.tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next/types";
 import React from "react";
 import satori from "satori";
 import fs from "fs";
+import { ImageResponse } from "@vercel/og";
 
 const openSans = fs.readFileSync(process.cwd() + "/fonts/OpenSans-Regular.ttf");
 const openSansSemiBold = fs.readFileSync(
@@ -17,56 +18,59 @@ export default async function handler(
   const queries = req.query as Record<string, string>;
 
   if (queries.type == "leetcode") {
-    const svg = await satori(
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          backgroundColor: "#111827",
-          padding: "1rem 3rem",
-          borderRadius: "10px",
-          position: "relative",
-          display: "flex",
-          flexDirection: "column",
-          color: "white",
-          fontFamily: "Open Sans",
-        }}
-      >
-        <h1
-          style={{
-            fontSize: "32px",
-            fontWeight: "600",
-          }}
-        >
-          {queries.name}
-        </h1>
-        <p
-          style={{
-            color: "#d1d5db",
-            margin: "5px 0",
-          }}
-        >
-          {queries.topics}
-        </p>
-        <p
-          style={{
-            color: "#d1d5db",
-            margin: "5px 0",
-          }}
-        >
-          {queries.difficulty}
-        </p>
+    res.setHeader("Content-Type", "image/png");
+    return new ImageResponse(
+      (
         <div
           style={{
-            position: "absolute",
-            bottom: "2rem",
-            right: "3rem",
+            height: "100%",
+            width: "100%",
+            backgroundColor: "#111827",
+            padding: "1rem 3rem",
+            borderRadius: "10px",
+            position: "relative",
             display: "flex",
+            flexDirection: "column",
+            color: "white",
+            fontFamily: "Open Sans",
           }}
         >
-          <img src="https://isatvik.com/favicon.ico" alt="" />
+          <h1
+            style={{
+              fontSize: "32px",
+              fontWeight: "600",
+            }}
+          >
+            {queries.name}
+          </h1>
+          <p
+            style={{
+              color: "#d1d5db",
+              margin: "5px 0",
+            }}
+          >
+            {queries.topics}
+          </p>
+          <p
+            style={{
+              color: "#d1d5db",
+              margin: "5px 0",
+            }}
+          >
+            {queries.difficulty}
+          </p>
+          <div
+            style={{
+              position: "absolute",
+              bottom: "2rem",
+              right: "3rem",
+              display: "flex",
+            }}
+          >
+            <img src="https://isatvik.com/favicon.ico" alt="" />
+          </div>
         </div>
-      </div>,
+      ),
       {
         width: 600,
         height: 337.5,
@@ -86,9 +90,6 @@ export default async function handler(
         ],
       }
     );
-
-    res.setHeader("Content-Type", "image/svg+xml");
-    return res.status(200).send(svg);
   }
 
   res.status(404).send("");
