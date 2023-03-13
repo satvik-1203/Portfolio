75888adb06e681202d0e62ef95f1b2b4555e3fb6
satvik-1203 <satvikgsu@gmail.com>
2023-03-13 16:58:43 -0400

    added changed

--- pages/api/dummy.ts
+++ pages/api/dummy.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
+
+  res.status(200).send("success");
 }
